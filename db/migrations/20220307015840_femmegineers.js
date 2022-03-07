32fae82cf8474b867e19f9277a44000dd57b4433
Jessica Duell <jessica.duell@pendo.io>
2022-03-06 21:55:08 -0500

    added march pairings'

--- db/migrations/20220307015840_femmegineers.js
+++ db/migrations/20220307015840_femmegineers.js
@@ -13,10 +13,6 @@ exports.up = function(knex) {
     });
 };
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
 exports.down = function(knex) {
   return knex.schema
     .dropTableIfExists("femmegineers")
--- db/seeds/03-pairings.js
+++ db/seeds/03-pairings.js
@@ -1,13 +1,17 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> } 
- */
 exports.seed = async function(knex) {
-  // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
-    {id: 1, colName: 'rowValue1'},
-    {id: 2, colName: 'rowValue2'},
-    {id: 3, colName: 'rowValue3'}
+  await knex('pairings').insert([
+    {year: 2021, month: "March", pair1: 1, pair2: 8},
+    {year: 2021, month: "March", pair1: 2, pair2: 12},
+    {year: 2021, month: "March", pair1: 3, pair2: 5},
+    {year: 2021, month: "March", pair1: 4, pair2: 18},
+    {year: 2021, month: "March", pair1: 6, pair2: 15},
+    {year: 2021, month: "March", pair1: 7, pair2: 20},
+    {year: 2021, month: "March", pair1: 9, pair2: 23},
+    {year: 2021, month: "March", pair1: 10, pair2: 13},
+    {year: 2021, month: "March", pair1: 11, pair2: 21},
+    {year: 2021, month: "March", pair1: 14, pair2: 24},
+    {year: 2021, month: "March", pair1: 16, pair2: 22},
+    {year: 2021, month: "March", pair1: 17, pair2: 19},
+    {year: 2021, month: "March", pair1: 25, pair2: 26},
   ]);
 };
